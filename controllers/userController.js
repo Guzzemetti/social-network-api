8fc8e6cb8b3e4c0f04af21d1082ecc500d13329d
John Guzzetta <johnguzzettaiv@gmail.com>
2022-05-22 13:52:21 -0400

    Updated userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,67 @@
 const { User, Thought } = require("../models");
 
 
+const userTotal = async () =>
+    User.aggregate()
+    .count("userCount")
+    .then((totalUsers) => totalUsers)
+
+
 // ********************* create crud commands to grab info *********************
 const userController = {
     // ********************* CRUD your User *********************
     // ********************* Reference the studentController *********************
+
+    // Get all users and display the total number of users
+    getUsers(req, res){
+        User.find()
+        .then(async (users) => {
+            const userObj = {
+                users,
+                userTotal: await userTotal(),
+            };
+            return res.json(userObj);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+
+    getSingleUser(req, res){
+        User.find({_id: req.params.userId})
+        .select("-__v")
+        .then(async (student) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+    }
+
+    createUser(req, res){
+
+    }
+
+
+    updateUser(req, res){
+        
+    }
+
+    deleteUser(req, res){
+        
+    }
+
+    addFriend(req, res){
+        
+    }
+
+    removeFriend(req, res){
+        
+    }
 }
 
 
